8e00381ea17f78cd8ba1cf36a036bd6875248b20
uvite <buniwota@gmail.com>
2023-05-21 22:01:30 +0800

    add

---
+++ deploy/dist/assets/operlog.81bbeb98.js
@@ -0,0 +1,1 @@
+import{A as o}from"./index.75c81e14.js";function t(e){return o({url:"/monitor/operlog/list",method:"get",params:e})}function l(e){return o({url:"/monitor/operlog/"+e,method:"delete"})}function n(){return o({url:"/monitor/operlog/clean",method:"delete"})}export{n as c,l as d,t as l};
