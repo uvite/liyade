7d4c62041dde9d785bfbc9c8c9800be04e520a10
uvite <buniwota@gmail.com>
2023-06-01 18:22:41 +0800

    add

--- deploy/dist/assets/index.5d87cf7a.js
+++
@@ -1,1 +0,0 @@
-import{r as o,o as r,c as t}from"./index.b0491df9.js";const p={__name:"index",setup(a){const e=o("/api/doc.html");return window.open(e.value),(c,n)=>(r(),t("div"))}};export{p as default};
--- deploy/dist/assets/index.b0fec777.js
+++
@@ -1,1 +0,0 @@
-import{r as o,o as r,c as t}from"./index.cf4206af.js";const p={__name:"index",setup(a){const e=o("/api/doc.html");return window.open(e.value),(c,n)=>(r(),t("div"))}};export{p as default};
